c1dfb6bbf52465650f74af9c958fc0606ef70ac3
Pavan Kumar Patruni <>
2019-08-19 19:06:28 +0530

    rxandroid integrated

--- app/src/main/java/com/pavankumarpatruni/pollingwidget/models/PollingImageItem.kt
+++
@@ -1,9 +0,0 @@
-package com.pavankumarpatruni.pollingwidget.models
-
-data class PollingImageItem(
-    val question: String,
-    val answers: List<AnswerImage>,
-    val selectedAnswer: Int
-)
-
-data class AnswerImage(val answer: String, val img: String, val count: Int, val total: Int)
--- app/src/main/java/com/pavankumarpatruni/pollingwidget/models/PollingItem.kt
+++
@@ -1,5 +0,0 @@
-package com.pavankumarpatruni.pollingwidget.models
-
-data class PollingItem(val question: String, val answers: List<Answer>, val selectedAnswer: Int)
-
-data class Answer(val answer: String, val count: Int, val total: Int)
